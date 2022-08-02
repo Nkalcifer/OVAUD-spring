a87e2ec3d3db10c93f23bdf35eaadd97065cc14b
Neider <ricsonearest@gmail.com>
2022-08-01 22:53:10 -0500

    Prueba Commit en Editor

--- OvaUdistrital/src/main/java/com/ova/app/OvaUdistritalApplication.java
+++ OvaUdistrital/src/main/java/com/ova/app/OvaUdistritalApplication.java
@@ -8,6 +8,7 @@ public class OvaUdistritalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OvaUdistritalApplication.class, args);
+		System.out.println("Hola");
 	}
 
 }
