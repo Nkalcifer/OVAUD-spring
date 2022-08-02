6f6bd9c75f21e2606d90001cf2dd6d1800d31c59
Neider <ricsonearest@gmail.com>
2022-08-01 22:58:13 -0500

    Original

--- OvaUdistrital/src/main/java/com/ova/app/OvaUdistritalApplication.java
+++ OvaUdistrital/src/main/java/com/ova/app/OvaUdistritalApplication.java
@@ -8,7 +8,6 @@ public class OvaUdistritalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OvaUdistritalApplication.class, args);
-		System.out.println("Hola");
 	}
 
 }
