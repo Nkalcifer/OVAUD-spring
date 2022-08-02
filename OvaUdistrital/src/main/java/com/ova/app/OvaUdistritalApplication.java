164cd60087e5ebac4e26505769624480f9777b3c
Neider <ricsonearest@gmail.com>
2022-08-01 22:49:07 -0500

    Configuración Base de Proyecto. Close #1

---
+++ OvaUdistrital/src/main/java/com/ova/app/OvaUdistritalApplication.java
@@ -0,0 +1,13 @@
+package com.ova.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvaUdistritalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvaUdistritalApplication.class, args);
+	}
+
+}
---
+++ OvaUdistrital/src/test/java/com/ova/app/OvaUdistritalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ova.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvaUdistritalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
